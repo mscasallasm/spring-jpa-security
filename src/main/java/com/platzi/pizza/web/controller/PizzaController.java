adda5e875f9293fdd3d316313876e2be19b694e3
mscasallasm <stiven_casallas@hotmail.com>
2024-02-03 11:38:11 -0500

    Clase 9: Borrando registros

--- src/main/java/com/platzi/pizza/web/controller/PizzaController.java
+++ src/main/java/com/platzi/pizza/web/controller/PizzaController.java
@@ -35,13 +35,22 @@ public class PizzaController {
         }
 
         return ResponseEntity.badRequest().build();
-
     }
 
     @PutMapping
     public ResponseEntity<PizzaEntity> update(@RequestBody PizzaEntity pizza) {
         if (pizza.getIdPizza() != null && this.pizzaService.exists(pizza.getIdPizza())){
             return ResponseEntity.ok(this.pizzaService.save(pizza));
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/{idPizza}")
+    public ResponseEntity<Void> delete(@PathVariable int idPizza) {
+        if (this.pizzaService.exists(idPizza)) {
+            this.pizzaService.delete(idPizza);
+            return ResponseEntity.ok().build();
         }
 
         return ResponseEntity.badRequest().build();
