1dad7761600bd95e2911c89c6c7d69790ea1df26
mscasallasm <stiven_casallas@hotmail.com>
2024-02-03 11:27:26 -0500

    Clase 8: Crear un nuevo registro con CrudRepository. Se realiza la opción de crear y de actualizar

--- src/main/java/com/platzi/pizza/web/controller/PizzaController.java
+++ src/main/java/com/platzi/pizza/web/controller/PizzaController.java
@@ -4,10 +4,7 @@ import com.platzi.pizza.persistence.entity.PizzaEntity;
 import com.platzi.pizza.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,24 @@ public class PizzaController {
     @GetMapping("/{idPizza}")
     public ResponseEntity<PizzaEntity> get(@PathVariable int idPizza) {
         return ResponseEntity.ok(this.pizzaService.get(idPizza));
+    }
+
+    @PostMapping
+    public ResponseEntity<PizzaEntity> add(@RequestBody PizzaEntity pizza) {
+        if (pizza.getIdPizza() == null || !this.pizzaService.exists(pizza.getIdPizza())){
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        }
+
+        return ResponseEntity.badRequest().build();
+
+    }
+
+    @PutMapping
+    public ResponseEntity<PizzaEntity> update(@RequestBody PizzaEntity pizza) {
+        if (pizza.getIdPizza() != null && this.pizzaService.exists(pizza.getIdPizza())){
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 }
