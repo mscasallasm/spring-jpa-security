1551d2165d83da7914fa301dd076620443540346
mscasallasm <stiven_casallas@hotmail.com>
2024-02-04 10:10:58 -0500

    Clase 11: Query methods, And, Or, True, IgnoreCase, OrderBy

--- src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
@@ -3,5 +3,10 @@ package com.platzi.pizza.persistence.repository;
 import com.platzi.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+
+    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
 }
