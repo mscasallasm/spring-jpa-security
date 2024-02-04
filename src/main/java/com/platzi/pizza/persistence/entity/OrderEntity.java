72d38543fa36735231a8057e397bbdf68b9fbbd6
mscasallasm <stiven_casallas@hotmail.com>
2024-02-04 09:58:05 -0500

    Clase 10: Usar Lazy y Eager al cargar relaciones, ayuda mucho a controlar las consultas que a veces son innecesarias.

--- src/main/java/com/platzi/pizza/persistence/entity/OrderEntity.java
+++ src/main/java/com/platzi/pizza/persistence/entity/OrderEntity.java
@@ -1,12 +1,19 @@
 package com.platzi.pizza.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "pizza_order")
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,11 +35,12 @@ public class OrderEntity {
     @Column(name = "additional_notes", length = 200)
     private String additionalNotes;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_customer", referencedColumnName = "id_customer", insertable = false, updatable = false)
+    @JsonIgnore
     private CustomerEntity customer;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", fetch = FetchType.EAGER)
     private List<OrderItemEntity> items;
 
 }
