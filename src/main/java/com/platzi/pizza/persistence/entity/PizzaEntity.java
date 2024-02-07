ff92762549831767f08521081bf265780e2c5fe4
mscasallasm <stiven_casallas@hotmail.com>
2024-02-07 10:30:58 -0500

    Clase 22: Auditoria con Spring Data, en este caso se crea una super clase y se le asignan los dos nuevos campos y asi no se modifican los compos de la entidad pizza, solo se agrega una anotacion y se extendiende la nueva clase

--- src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
+++ src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
@@ -4,13 +4,15 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity {
+public class PizzaEntity extends AuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pizza", nullable = false)
