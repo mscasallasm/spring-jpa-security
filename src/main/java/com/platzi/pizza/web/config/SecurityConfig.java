7856fb65ca6ae195f31743845a230d2c7d29670f
mscasallasm <stiven_casallas@hotmail.com>
2024-02-14 11:11:24 -0500

    Clase 9: Aplicar requestMatchers. Con esto se definen reglas que hacen que se permita o se deniege el acceso a diferentes endpoints o metodos http

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.platzi.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(customizeRequests -> {
                             customizeRequests
+                                    .requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                                    .requestMatchers(HttpMethod.PUT).denyAll()
                                     .anyRequest()
                                     .authenticated();
                         }
